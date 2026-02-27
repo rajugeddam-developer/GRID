88274e93d2082214e140256877df9f95a819024c
rajugeddam-developer <rajugeddam18@gmail.com>
2026-02-27 15:13:31 +0530

    fix: removed deleted html files from vite build config options

--- vite.config.js
+++ vite.config.js
@@ -18,10 +18,7 @@ export default defineConfig({
                 activities: resolve(__dirname, 'activities.html'),
                 dashboard: resolve(__dirname, 'dashboard.html'),
                 events: resolve(__dirname, 'events.html'),
-                login: resolve(__dirname, 'login.html'),
                 materials: resolve(__dirname, 'materials.html'),
-                profile: resolve(__dirname, 'profile.html'),
-                register: resolve(__dirname, 'register.html'),
                 team: resolve(__dirname, 'team.html'),
                 team_directory: resolve(__dirname, 'team_directory.html')
             }
